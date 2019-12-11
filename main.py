57568b032d0ff7aa43d1df84e51d5ff377bff991
buitim <buitim@oregonstate.edu>
2019-12-11 13:53:26 -0800

    Added comment explanations

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydub.playback import play
 
 # https://stackoverflow.com/questions/53633177/how-to-read-a-mp3-audio-file-into-a-numpy-array-save-a-numpy-array-to-mp3?noredirect=1&lq=1
 def read(f, normalized=True):
-    """MP3 to numpy array"""
+    """ MP3 to numpy array """
     a = pydub.AudioSegment.from_mp3(f)
     y = np.array(a.get_array_of_samples())
     if a.channels == 2:
@@ -16,8 +16,9 @@ def read(f, normalized=True):
     else:
         return a.frame_rate, y
 
+# https://stackoverflow.com/questions/53633177/how-to-read-a-mp3-audio-file-into-a-numpy-array-save-a-numpy-array-to-mp3?noredirect=1&lq=1
 def write(f, sr, x, normalized=True):
-    """numpy array to MP3"""
+    """ numpy array to MP3 """
     channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
     if normalized:  # normalized array - each item should be a float in [-1, 1)
         y = np.int16(x * 2 ** 15)
@@ -28,14 +29,27 @@ def write(f, sr, x, normalized=True):
 
 def main():
 	print("[+] Importing audio...")
+
+	# Replace 'testclip.mp3' with the name of the audio file in the root directory
 	rate, data = read('testclip.mp3')
+
+	# Uncomment the following line if using a wav file and change the name of the file to a wav file in the root directory
 	# rate, data = sp.io.wavfile.read("testclip.wav")
+
 	print("[+] Length of clip array:", len(data))
+
+	# Noise is the section of audio or audio clip of noise. 
+	# Here, the audio file begins with background noise before getting into the actual audio.
+	# Hence, I grab some audio from the beginning of the clip to use as a noise profile.
+	# Alternatively, you could have a separate audio file to use as a noise profile. 
 	print("[+] Getting noise...")
 	noise = data[0:140000]
+
+	# Perform the noise reduction using the noisereduce library
 	print("[+] Reducing noise...")
 	reducedNoise = nr.reduce_noise(audio_clip=data, noise_clip=noise, verbose=True)
 	
+	# Exports noise reduced output to 'out.mp3'
 	# write('out.mp3', rate, data)
 
 main()
